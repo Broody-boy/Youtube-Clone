1faeaeb2e0bf6af3102be4b7fb66e04fb917a74f
Arshdeep <arshdeep2875@gmail.com>
2024-09-11 17:57:18 +0530

    added incipient Videos component without API call

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from 'react'
 import { Box, Stack, Typography } from '@mui/material'
-import Sidebar from './Sidebar'
+import { Sidebar, Videos } from './'
 
 const Feed = () => {
   return (
@@ -24,11 +24,21 @@ const Feed = () => {
         }
       }}>
 
-        <Sidebar/>
+        <Sidebar />
 
         <Typography className='copyright' variant='body2' sx = {{ mt: 1.5, color: '#fff' }}>
           Copyright 2024 Arshdeep Singh
         </Typography>
+
+      </Box>
+
+      <Box p={2} sx={{ overflowY: 'auto', height: '90vh', flex: 2 }}>
+        
+        <Typography variant='h4' fontWeight='bold' mb = {2} sx={{ color: 'white' }}>
+          New <span style = {{color: '#fc1503'}}>videos</span>
+        </Typography>
+
+        <Videos videos={[]} />
 
       </Box>
 
