3fe4438c8d737c6707127b0eb9e8ccb75c864bb1
Arshdeep <arshdeep2875@gmail.com>
2024-09-21 02:17:01 +0530

    Fixed few screen size related issues

--- src/components/VideoCard.jsx
+++ src/components/VideoCard.jsx
@@ -8,12 +8,12 @@ const VideoCard = ({ video: { id: { videoId }, snippet } }) => {
   // console.log('videoId', videoId)
   // console.log('snippet', snippet)
   return (
-    <Card sx={{ width: { md: '320px', xs: '100%'}, boxShadow: 'none', borderRadius: 0 }}>
+    <Card sx={{ width: { xs: '100%', sm:'358px', md: '320px' }, boxShadow: 'none', borderRadius: 0 }}>
       <Link to={ videoId ? `/video/${videoId}` : demoVideoUrl }>
         <CardMedia
           image={snippet?.thumbnails?.high?.url}
           alt={snippet?.title}
-          sx={{ width: 358, height: 180 }}
+          sx={{ width: { xs: '100%', sm: '358px', md: '320px' }, height: 180 }}
         />
 
         <CardContent sx={{ backgroundColor: '#1e1e1e', height: '106px' }}>
@@ -27,7 +27,7 @@ const VideoCard = ({ video: { id: { videoId }, snippet } }) => {
           <Link to={ snippet?.channelId ? `/channel/${snippet?.channelId}` : demoChannelUrl }>
             <Typography variant='subtitle2' fontWeight='bold' color='gray'>
               {snippet?.channelTitle || demoChannelTitle}
-              <CheckCircle sx={{ fontSize: 12, color: 'gray', ml: '5px'}} />
+              <CheckCircle sx={{ fontSize: 12, color: 'gray', ml: '5px' }} />
             </Typography>
           </Link>
 
